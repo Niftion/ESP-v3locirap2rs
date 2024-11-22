0ea470af10be3723b4e2b167e76d5a4cfd10c0c7
= <craig.alex.kelley@gmail.com>
2024-11-21 21:59:55 -0800

    blinking light code

--- software/ESP-v3locirap2rs_sofware/src/main.cpp
+++ software/ESP-v3locirap2rs_sofware/src/main.cpp
@@ -1,19 +1,17 @@
 #include <Arduino.h>
-#include <Wire.h>
+// #include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 2
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	// setup pins
+	pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	// flash LED
+	digitalWrite(LED_PIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_PIN, LOW);
+	delay(1000);
 }
